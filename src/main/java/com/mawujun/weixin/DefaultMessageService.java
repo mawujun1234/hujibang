cda0ff543951e638bf35c63821189e4c995800c7
mawujn1234 <mawujun1234@163.com>
2015-10-11 19:03:00 +0800

    ll

--- src/main/java/com/mawujun/weixin/DefaultMessageService.java
+++ src/main/java/com/mawujun/weixin/DefaultMessageService.java
@@ -3,9 +3,9 @@ package com.mawujun.weixin;
 import com.mawujun.controller.spring.SpringContextHolder;
 import com.mawujun.messge.service.IRequestProcess;
 import com.mawujun.messge.service.IResponseProcess;
-import com.mawujun.messge.service.MessageService;
+import com.mawujun.messge.service.AbstractMessageService;
 
-public class DefaultMessageService extends MessageService {
+public class DefaultMessageService extends AbstractMessageService {
 
 	@Override
 	public IRequestProcess getRequestProcess() {
